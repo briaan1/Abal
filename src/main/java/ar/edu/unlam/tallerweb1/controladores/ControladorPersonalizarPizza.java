afe72c647d3d5987d02c0982bb15619bf6b575f5
andrea-nd <descous.andrea@hotmail.com>
2022-05-29 18:31:04 -0300

    ControladorPersonalizarPizzaTest

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPersonalizarPizza.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorPersonalizarPizza.java
@@ -1,13 +1,38 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Producto;
+import ar.edu.unlam.tallerweb1.servicios.ServicioCategoriaPizza;
+
 @Controller
+
 public class ControladorPersonalizarPizza {
+	private ServicioCategoriaPizza servicioCategoriaPizza;
+	
+	@Autowired
+	public ControladorPersonalizarPizza(ServicioCategoriaPizza servicioCategoriaPizza) {
+		this.servicioCategoriaPizza = servicioCategoriaPizza;
+	}
+
 	@RequestMapping("/personalizar-pizza")
 	public ModelAndView irAPersonalizarPizza() {
-		return new ModelAndView("personalizarPizza");
+		ModelMap model = new ModelMap();
+		List<Producto> listaDeProducto = servicioCategoriaPizza.getListaDeProductos("pizza");
+		if(listaDeProducto!=null) {
+			model.put("listaDePizza", listaDeProducto);
+		}else {
+			model.put("mensaje", "No hay productos para mostrar");
+		}
+		
+		return new ModelAndView("personalizarPizza",model);
 	}
+	
+	
 }
--- src/test/java/ar/edu/unlam/tallerweb1/controlador/ControladorPersonalizarPizzaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controlador/ControladorPersonalizarPizzaTest.java
@@ -1,10 +1,79 @@
 package ar.edu.unlam.tallerweb1.controlador;
 
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import ar.edu.unlam.tallerweb1.controladores.ControladorPersonalizarPizza;
+import ar.edu.unlam.tallerweb1.modelo.Categoria;
+import ar.edu.unlam.tallerweb1.modelo.Producto;
+import ar.edu.unlam.tallerweb1.servicios.ServicioCategoriaPizza;
 
 public class ControladorPersonalizarPizzaTest {
-	private ControladorPersonalizarPizza controladorPersonalizarPizza;
+	private ServicioCategoriaPizza servicioCategoriaPizza= mock(ServicioCategoriaPizza.class);
+	private ControladorPersonalizarPizza controladorPersonalizarPizza= new ControladorPersonalizarPizza(servicioCategoriaPizza);
 	
+	@Test
+	public void alPedirPersonalizarQueMeMuestrePersonalizarPizza() {
+		ModelAndView model = cuandoPidoPersonalizarPizza();
+		entoncesMeLlevaALaVista("personalizarPizza",model);
+	}
+
+	private void entoncesMeLlevaALaVista(String vistaEsperada, ModelAndView model) {
+		assertThat(model.getViewName()).isEqualTo(vistaEsperada);	
+	}
+
+	private ModelAndView cuandoPidoPersonalizarPizza() {
+		return controladorPersonalizarPizza.irAPersonalizarPizza();
+	}
+
 	
+	@Test
+	public void alPedirLaListaEntoncesMeMuestraLaListaDePizzas() {
+		dadoQueExisteUnaListaDePizza("pizza",4);
+		ModelAndView model = cuandoPidoLaLista("pizza");
+		entoncesMeMuestraLaListaDePizza(4,model);
+	}
+
+	private void entoncesMeMuestraLaListaDePizza(int cantidadEsperada, ModelAndView model) {
+		assertThat(model.getModel().get("listaDePizza")).asList().hasSize(cantidadEsperada);
+	}
+
+	private ModelAndView cuandoPidoLaLista(String string) {
+		
+		return controladorPersonalizarPizza.irAPersonalizarPizza();
+	}
+
+	private void dadoQueExisteUnaListaDePizza(String nombreCategoria, int cantidadDePizzas) {
+		List<Producto> listaDeProductos=new ArrayList<Producto>();
+		for(int i=0;i<cantidadDePizzas;i++) {
+			listaDeProductos.add(new Producto());
+		}
+		when(servicioCategoriaPizza.getListaDeProductos(nombreCategoria)).thenReturn(listaDeProductos);		
+	}
+	
+	@Test
+	public void mostrarUnMensajeSiNoHayPizzas() {
+		dadoQueNoHayPizzas("pizza");
+		ModelAndView model = cuandoPidoLaLista("pizza");
+		entoncesMeMuestraUnMensaje("No hay productos para mostrar",model);
+	}
+
+	private void entoncesMeMuestraUnMensaje(String mensajeEsperado, ModelAndView model) {
+		assertThat(model.getModel().get("mensaje")).isEqualTo(mensajeEsperado);	
+	}
+
+	private void dadoQueNoHayPizzas(String nombreCategoria) {
+		
+		when(servicioCategoriaPizza.getListaDeProductos(nombreCategoria)).thenReturn(null);	
+		
+	}
 
 }
