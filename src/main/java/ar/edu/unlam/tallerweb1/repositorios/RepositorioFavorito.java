55885b7c5a75d308df6e600aaa051194315e4577
andrea-nd <descous.andrea@hotmail.com>
2022-06-14 16:46:43 -0300

    Repositorio y ServicioTest de Favorito

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioFavorito.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Favorito;
+import ar.edu.unlam.tallerweb1.modelo.Producto;
+
+public interface RepositorioFavorito {
+
+	List<Favorito> listarFavoritos();
+}
