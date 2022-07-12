7e529e777f27e5c01f7edd0e0ae6130116845a26
Gomez Angeles <92001030+AGomz@users.noreply.github.com>
2022-07-12 01:43:46 -0300

    Se elimino archivo subido por error

--- src/main/webapp/js/localStorage.js
+++
@@ -1,9 +0,0 @@
-var contador = document.getElementById("contadorCarrito").value;
-
-localStorage.setItem("countChart", contador);
-
-var resultadoContador = localStorage.getItem("countChart");
-
-//console.log(resultadoContador);
-
-document.getElementById("mostrarContador").innerHTML = resultadoContador;
